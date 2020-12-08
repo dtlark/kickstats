2d76e3f0a076d7344a7b7b3485ad632b21f73753
ProjectJFrost <projectjfrost@gmail.com>
2020-12-07 20:37:54 -0500

    Updated the SQL

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.post('/query', (req, res) => {
     switch(req.body.interests){
         case 'averageAmount':
             sql = `SELECT ROUND(AVG(avgDonate), 2) AS AVG_BACKER_MONTH
-            FROM(SELECT EXTRACT(MONTH FROM launch_date) AS months, avgDonate
+            FROM(SELECT EXTRACT(MONTH FROM launch_date) AS months, EXTRACT(YEAR FROM launch_date) AS years, avgDonate
                 FROM(SELECT launch_date, pledge.average_donation AS avgDonate
                     FROM pledge, hosts,
                         (SELECT id AS CID
@@ -28,12 +28,12 @@ app.post('/query', (req, res) => {
                     AND hosts.id = CID
                     AND launch_date >= to_date('${req.body.dateStart}', 'yyyy-mm')
                     AND launch_date <= to_date('${req.body.dateEnd}', 'yyyy-mm')))
-            GROUP BY months
-            ORDER BY months`
+            GROUP BY years, months
+            ORDER BY years, months`
             break;
         case 'averageRaised':
             sql = `SELECT ROUND(AVG(pledge_amount), 2) AS monthAvg
-                FROM(SELECT EXTRACT(MONTH FROM launch_date) AS months, pledge_amount
+                FROM(SELECT EXTRACT(MONTH FROM launch_date) AS months, EXTRACT(YEAR FROM launch_date) AS years, pledge_amount
                     FROM(SELECT launch_date, pledge_amount
                             FROM hosts, pledge, kickstarter_project,
                                  (SELECT id AS CID
@@ -45,12 +45,12 @@ app.post('/query', (req, res) => {
                             AND pledge.id = CID
                             AND kickstarter_project.id = CID
                             AND kickstarter_project.state = 'successful'))
-                GROUP BY months
-                ORDER BY months`
+                GROUP BY years, months
+                ORDER BY years, months`
             break;
         case 'numFailed':
             sql = `SELECT COUNT(*)
-            FROM(SELECT EXTRACT(MONTH FROM launch_date) AS months
+            FROM(SELECT EXTRACT(MONTH FROM launch_date) AS months, EXTRACT(YEAR FROM launch_date) AS years
                     FROM(SELECT CID, launch_date
                             FROM hosts,
                                  kickstarter_project,
@@ -63,12 +63,12 @@ app.post('/query', (req, res) => {
                             AND kickstarter_project.id = CID
                             AND kickstarter_project.state != 'successful'
                     AND kickstarter_project.state != 'live'))
-            GROUP BY months
-            ORDER BY months`
+            GROUP BY years, months
+            ORDER BY years, months`
             break;
         case 'numInvestors':
             sql = `SELECT SUM(num_of_backers) AS backers 
-            FROM(SELECT EXTRACT(MONTH FROM launch_date) as months, num_of_backers
+            FROM(SELECT EXTRACT(MONTH FROM launch_date) as months, EXTRACT(YEAR FROM launch_date) AS years, num_of_backers
                 FROM(SELECT CID, launch_date, num_of_backers
                     FROM hosts,
                          pledge,
@@ -79,12 +79,12 @@ app.post('/query', (req, res) => {
                     AND launch_date >= to_date('${req.body.dateStart}', 'yyyy-mm')
                     AND launch_date <= to_date('${req.body.dateEnd}', 'yyyy-mm')
                     AND pledge.id = CID))
-            GROUP BY months
-            ORDER BY months`
+            GROUP BY years, months
+            ORDER BY years, months`
             break;
         case 'num200':
             sql = `SELECT COUNT(*)
-            FROM(SELECT EXTRACT(MONTH FROM launch_date) AS months
+            FROM(SELECT EXTRACT(MONTH FROM launch_date) AS months, EXTRACT(YEAR FROM launch_date) AS years
                 FROM(SELECT launch_date, pledge_amount, (goal * 2) AS SuperFund
                     FROM pledge, hosts, kickstarter_project,
                         (SELECT id AS CID
@@ -96,12 +96,12 @@ app.post('/query', (req, res) => {
                     AND launch_date >= to_date('${req.body.dateStart}', 'yyyy-mm')
                     AND launch_date <= to_date('${req.body.dateEnd}', 'yyyy-mm'))
                 WHERE pledge_amount >= SuperFund)
-            GROUP BY months
-            OrDER BY months`
+            GROUP BY years, months
+            OrDER BY years, months`
             break;
         case 'numStarted':
             sql = `SELECT COUNT(*)
-            FROM(SELECT EXTRACT(MONTH FROM launch_date) AS months
+            FROM(SELECT EXTRACT(MONTH FROM launch_date) AS months, EXTRACT(YEAR FROM launch_date) AS years
                     FROM(SELECT CID, launch_date
                             FROM hosts,
                                 (SELECT id AS CID
@@ -110,12 +110,12 @@ app.post('/query', (req, res) => {
                             WHERE hosts.id = CID)
                             WHERE launch_date >= to_date('${req.body.dateStart}', 'yyyy-mm')
                             AND launch_date <= to_date('${req.body.dateEnd}', 'yyyy-mm'))
-            GROUP BY months
-            ORDER BY months`
+            GROUP BY years, months
+            ORDER BY years, months`
             break;
         case 'numSuccess':
             sql = `SELECT COUNT(*)
-            FROM(SELECT EXTRACT(MONTH FROM launch_date) AS months
+            FROM(SELECT EXTRACT(MONTH FROM launch_date) AS months, EXTRACT(YEAR FROM launch_date) AS years
                     FROM(SELECT CID, launch_date
                             FROM hosts,
                                  kickstarter_project,
@@ -127,13 +127,13 @@ app.post('/query', (req, res) => {
                             AND launch_date <= to_date('${req.body.dateEnd}', 'yyyy-mm')
                             AND kickstarter_project.id = CID
                             AND kickstarter_project.state = 'successful'))
-            GROUP BY months
-            ORDER BY months`
+            GROUP BY years, months
+            ORDER BY years, months`
             break;
         case 'successRate':
             sql = `SELECT ROUND(((successes / total) * 100), 2) AS SuccessRate
-            FROM(SELECT COUNT(*) AS successes, months
-                FROM(SELECT EXTRACT(MONTH FROM launch_date) AS months
+            FROM(SELECT COUNT(*) AS successes, months, years
+                FROM(SELECT EXTRACT(MONTH FROM launch_date) AS months, EXTRACT(YEAR FROM launch_date) AS years
                         FROM(SELECT CID, launch_date
                                 FROM hosts,
                                      kickstarter_project,
@@ -145,9 +145,9 @@ app.post('/query', (req, res) => {
                                 AND launch_date <= to_date('${req.body.dateEnd}', 'yyyy-mm')
                                 AND kickstarter_project.id = CID
                                 AND kickstarter_project.state = 'successful'))
-                GROUP BY months),
-                (SELECT COUNT(*) AS total, tmonths
-                    FROM(SELECT EXTRACT(MONTH FROM launch_date) AS tmonths
+                GROUP BY years, months),
+                (SELECT COUNT(*) AS total, tmonths, tyears
+                    FROM(SELECT EXTRACT(MONTH FROM launch_date) AS tmonths, EXTRACT(YEAR FROM launch_date) AS tyears
                             FROM(SELECT CID, launch_date
                                     FROM hosts,
                                         (SELECT id AS CID
@@ -156,9 +156,10 @@ app.post('/query', (req, res) => {
                                     WHERE hosts.id = CID)
                             WHERE launch_date >= to_date('${req.body.dateStart}', 'yyyy-mm')
                             AND launch_date <= to_date('${req.body.dateEnd}', 'yyyy-mm'))
-                    GROUP BY tmonths)
+                    GROUP BY tyears, tmonths)
             WHERE tmonths = months
-            ORDER BY months`
+            AND tyears = years
+            ORDER BY years, months`
             break;
 
     }
